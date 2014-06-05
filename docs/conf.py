2002feec79cc9e3d76565ec206d97549a484b136
Jeremy Thurgood <jeremy@praekelt.com>
2014-06-06 00:34:06 +0200

    Revert "Figure out why the rtd git repo is dirty."
    This reverts commit 81d377b916f2933caf67fee27f7565f6389f03c2.

--- docs/conf.py
+++ docs/conf.py
@@ -12,21 +12,6 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-
-##########################
-##########################
-
-import os
-print "\nvvvvvvvvvvvvvvvvvvvvvvvvvv\n"
-os.system("git status")
-print "\n==========================\n"
-os.system("git diff")
-print "\n^^^^^^^^^^^^^^^^^^^^^^^^^^\n"
-
-##########################
-##########################
-
-
 import sys
 import os
 
